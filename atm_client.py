1aa76b80bd5c193b5f009fbd6dc5b176b4e2272a
Benjamin Kaija <Benjamin Kaija>
2015-12-07 12:46:09 -0500

    added first UI file

---
+++ atm_client.py
@@ -0,0 +1,12 @@
+from Tkinter import *
+
+
+class atm_client:
+	def __init__(self):
+		self.base_window = Tk()
+		self.base_window.title('ATM Machine')
+		
+		self.base_window.mainloop()
+
+
+client = atm_client()
